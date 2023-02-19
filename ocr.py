5850f76c2bf3ef8dc48b3ed88c73aacdb02630ec
mwestera <matthijs.westera@gmail.com>
2023-02-19 11:30:44 +0100

    slight refactoring; add failed second attempt

--- ocr.py
+++ ocr.py
@@ -1,3 +1,4 @@
+import PIL.ImageFilter
 import pytesseract
 from PIL import Image
 import re
@@ -27,24 +28,25 @@ def main():
             if item['type'] == 'example':
                 image_path = f'scraped/{item["image_file"]}'
                 print(image_path)
-                words, todi = todi_from_image(image_path)
-                if words:
-                    item['words_ocr'], item['todi_ocr'] = ' '.join(words), ' '.join(todi)
+                try:
+                    image = Image.open(image_path)
+                except Exception:
+                    continue
+                else:
+                    words_ocr, todi_ocr = todi_from_image(image)
+                    if words_ocr:
+                        item['words_ocr'] = words_ocr
+                    if todi_ocr:
+                        item['todi_ocr'] = todi_ocr
             json_writer.write(json.dumps(item) + '\n')
 
     jsonlines_to_csv(jsonlines_path)
 
 
 def todi_from_image(image):
-    try:
-        image = Image.open(image)
-    except Exception:
-        return None, None
-
     x, y = image.size
     image = image.crop((0, y - 40, x, y))
 
-    # TODO: Oooh:  config="-c tessedit_char_whitelist=HL%-!*"
     ocr_result = pytesseract.image_to_string(image, lang='nld')
 
     if len(ocr_result) < 5:
@@ -60,14 +62,41 @@ def todi_from_image(image):
             wordlines.append(line)
 
     todi = ' '.join(todilines)
-    words = ' '.join(wordlines)
+    words = ' '.join(' '.join(wordlines).strip().split())
     todi = clean_todi(todi)
-
-    words = words.split()
-    todi = todi.split()
 
     return words, todi
 
+
+# def convert_to_rgb(image):
+#     """Set transparent background to white."""
+#     image = image.convert('RGBA')
+#     new_image = Image.new("RGBA", image.size, "WHITE")
+#     new_image.paste(image, (0, 0), image)
+#     return new_image.convert('RGB')
+
+
+def todi_from_image_2(image):
+    """
+    I expected this version, using OCR whitelist option and separate
+    crops for todi vs. words, to work better, but it is much worse.
+    """
+    x, y = image.size
+    todi_region = image.crop((0, y - 30, x, y - 12))
+    words_region = image.crop((0, y - 12, x, y))
+
+    todi_ocr = pytesseract.image_to_string(todi_region, lang='eng', config="-c tessedit_char_whitelist='HL%!*()' -c preserve_interword_spaces=1")     # , config="-c tessedit_char_whitelist='HL%!*() '"
+    words_ocr = pytesseract.image_to_string(words_region, lang='nld', config="-c preserve_interword_spaces=1")
+
+    todi_ocr = ' '.join(todi_ocr.splitlines())
+    words_ocr = ' '.join(words_ocr.splitlines())
+
+    if not words_ocr.strip():
+        words_ocr = None
+    if not todi_ocr.strip():
+        todi_ocr = None
+
+    return words_ocr, todi_ocr
 
 
 todi_corrections = {
